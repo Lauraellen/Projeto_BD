ca6c859da4844627f9789a3531e00d84065ac1e7
DaianeDionisio <daianedionisio2000@gmail.com>
2022-12-03 14:46:03 -0300

    read

--- db/crud.py
+++ db/crud.py
@@ -4,6 +4,9 @@ class CRUD:
     def __init__(self):
         self.db = Graph("bolt://34.200.218.33:7687", "neo4j", "roof-money-hardcopies")
 
+
+
+#------------------------------------------FUNCTIONS CREATE------------------------------------------------
     def createPerson(self, nome, dataDeNascimento, cartaoDoSus, cpf):
         query = "CREATE (:Pessoa{nome: \"" + nome + "\", dataDeNascimento: \"" + dataDeNascimento + \
                 "\", cartaoDoSus: " + cartaoDoSus + ", cpf: " + cpf + "})"
@@ -23,3 +26,22 @@ class CRUD:
         query = "CREATE (:Fabricante{nome: \"" + nome + "\", sede: \"" + sede + \
                 "\", fundacao: " + fundacao + "})"
         self.db.execute_query(query)
+
+
+#------------------------------------------FUNCTIONS READ------------------------------------------------
+
+    def readPerson(self, nome):
+        query = "MATCH (p:Pessoa{nome: \"" + nome + "\"}) RETURN p"
+        print(self.db.execute_query(query))
+
+    def readPSF(self, numIdent):
+        query = "MATCH (s:PSF{numIdent: \"" + numIdent + "\"}) RETURN s"
+        print(self.db.execute_query(query))
+
+    def readVaccine(self, nome):
+        query = "MATCH (v:Vacina{nome: \"" + nome + "\"}) RETURN v"
+        print(self.db.execute_query(query))
+
+    def readManufacturer(self, nome):
+        query = "MATCH (f:Fabricante{nome: \"" + nome + "\"}) RETURN f"
+        print(self.db.execute_query(query))
