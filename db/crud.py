721856c80dcebffbab2dd5e1f01d7202f01dd3b9
DaianeDionisio <daianedionisio2000@gmail.com>
2022-12-03 21:06:32 -0300

    crud

--- db/crud.py
+++ db/crud.py
@@ -8,49 +8,49 @@ class CRUD:
 
     # ------------------------------------------FUNCTIONS CREATE------------------------------------------------
     def createPerson(self, nome, dataDeNascimento, cartaoDoSus, cpf):
-        query = "CREATE (:Pessoa{nome: $nome, dataDeNascimento: $dataDeNascimento, cartaoDoSus: $cartaoDoSus, +cpf: $cpf})"
+        query = "create (:Pessoa{nome: $nome, dataDeNascimento: $dataDeNascimento, cartaoDoSus: $cartaoDoSus, +cpf: $cpf})"
         self.db.execute_query(query, {'nome': nome, 'dataDeNascimento': dataDeNascimento, 'cartaoDoSus': cartaoDoSus, 'cpf': cpf})
 
     def createPSF(self, numIdent, endereco, cidade):
-        query = "CREATE (:PSF{numIdent: $numIdent, endereco: $endereco, cidade: $cidade})"
+        query = "create (:PSF{numIdent: $numIdent, endereco: $endereco, cidade: $cidade})"
         self.db.execute_query(query,{'numIdent': numIdent, 'endereco': endereco, 'cidade': cidade})
 
     def createVaccine(self, nome, eficacia, validade):
-        query = "CREATE (:Vacina{nome: $nome, eficacia: $eficacia, validade: $validade})"
+        query = "create (:Vacina{nome: $nome, eficacia: $eficacia, validade: $validade})"
         self.db.execute_query(query,{'nome': nome, 'eficacia': eficacia, 'validade': validade})
 
     def createManufacturer(self, nome, sede, fundacao):
-        query = "CREATE (:Fabricante{nome: $nome, sede: $sede, fundacao: $fundacao})"
+        query = "create (:Fabricante{nome: $nome, sede: $sede, fundacao: $fundacao})"
         self.db.execute_query(query,{'nome': nome, 'sede': sede, 'fundacao': fundacao})
 
     def createRelationshipPersonVaccine(self, nomePerson, nomeVaccine, lote):
-        query = "MATCH (p:Pessoa{nome: $nomePerson}), (v:Vacina{nome: $nomeVaccine}) CREATE (p)-[:RECEBE{lote: $lote}]->(v)"
+        query = "match (p:Pessoa{nome: $nomePerson}), (v:Vacina{nome: $nomeVaccine}) CREATE (p)-[:RECEBE{lote: $lote}]->(v)"
         self.db.execute_query(query,{'nomePerson': nomePerson, 'nomeVaccine': nomeVaccine, 'lote': lote})
 
     def createRelationshipPersonPSF(self, nomePerson, numIdentPSF):
-        query = "MATCH (p:Pessoa{nome: $nomePerson}), (s:PSF{numIdent: $numIdentPSF}) CREATE (p)-[:PERTENCE_A]->(S)"
+        query = "match (p:Pessoa{nome: $nomePerson}), (s:PSF{numIdent: $numIdentPSF}) CREATE (p)-[:PERTENCE_A]->(S)"
         self.db.execute_query(query, {'nomePerson': nomePerson, 'numIdentPSF': numIdentPSF})
 
     def createRelationshipVaccineManufacturer(self, nomeVaccine, nomeManufacturer):
-        query = "MATCH (v:Vacina{nome: $nomeVaccine}), (f:PSF{nome: $nomeManufacturer}) CREATE (v)-[:FABRICADA_POR]->(f)"
+        query = "match (v:Vacina{nome: $nomeVaccine}), (f:PSF{nome: $nomeManufacturer}) CREATE (v)-[:FABRICADA_POR]->(f)"
         self.db.execute_query(query,{'nomeVaccine': nomeVaccine,'nomeManufacturer': nomeManufacturer})
 
     # ------------------------------------------FUNCTIONS READ------------------------------------------------
 
     def readPerson(self, nome):
-        query = "MATCH (p:Pessoa{nome: $nome}) RETURN p"
+        query = "match (p:Pessoa{nome: $nome}) return p"
         print(self.db.execute_query(query,{'nome': nome}))
 
     def readPSF(self, numIdent):
-        query = "MATCH (s:PSF{numIdent: $numIdent}) RETURN s"
+        query = "match (s:PSF{numIdent: $numIdent}) return s"
         print(self.db.execute_query(query,{'numIdent': numIdent}))
 
     def readVaccine(self, nome):
-        query = "MATCH (v:Vacina{nome: $nome}) RETURN v"
+        query = "match (v:Vacina{nome: $nome})return v"
         print(self.db.execute_query(query,{'nome': nome}))
 
     def readManufacturer(self, nome):
-        query = "MATCH (f:Fabricante{nome: $nome}) RETURN f"
+        query = "MATCH (f:Fabricante{nome: $nome} return f"
         print(self.db.execute_query(query,{'nome': nome}))
 
     # ------------------------------------------FUNCTIONS UPDATE------------------------------------------------
@@ -66,17 +66,17 @@ class CRUD:
     # ------------------------------------------FUNCTIONS DELETE------------------------------------------------
 
     def deletePerson(self, nome):
-        query = "MATCH (p:Pessoa{nome: $nome}) DETACH DELETE p"
+        query = "match (p:Pessoa{nome: $nome}) detach delete p"
         self.db.execute_query(query,{'nome': nome})
 
     def deletePSF(self, numIdent):
-        query = "MATCH (s:PSF{numIdent: $numIdent}) DETACH DELETE s"
+        query = "match (s:PSF{numIdent: $numIdent}) detach delete s"
         self.db.execute_query(query,{'numIdent': numIdent})
 
     def deleteVaccine(self, nome):
-        query = "MATCH (v:Vacina{nome: $nome}) DETACH DELETE v"
+        query = "match (v:Vacina{nome: $nome}) detach delete v"
         self.db.execute_query(query,{'nome': nome})
 
     def deleteManufacturer(self, nome):
-         query = "MATCH (f:Fabricante{nome: $nome}) DETACH DELETE f"
+         query = "match (f:Fabricante{nome: $nome}) detach delete f"
          self.db.execute_query(query,{'nome': nome})
